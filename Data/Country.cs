4a6e17c2f86b3f018f74b72b3bd7e40758dc7422
devincar <devmcaron@gmail.com>
2025-09-20 11:42:11 -0700

    Added validation rules

--- Data/Country.cs
+++ Data/Country.cs
@@ -1,10 +1,14 @@
-﻿namespace HotelApi.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelApi.Data;
 
 public class Country
 {
     public int CountryId { get; set; }
-    public string Name { get; set; }
-    public string ShortName { get; set; }
+    [MaxLength(20)]
+    public required string Name { get; set; }
+    [MaxLength(3)]
+    public required string ShortName { get; set; }
     public IList<Hotel> Hotels { get; set; } = [];
 }
 
